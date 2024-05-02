a4360bc1465a686d9862dda33afe5d8eb7fb27f5
Jeykop22 <nolanbinet2@gmail.com>
2024-05-02 07:33:44 +0000

    pagination

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Range simple helper fun """
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Range simple helper fun
+    """
+
+    final_size: int = page * page_size
+    start_size: int = final_size - page_size
+
+    return (start_size, final_size)
