625e28fd5e21a5263f058d4acdbac7486918a621
Jeykop22 <nolanbinet2@gmail.com>
2024-05-21 11:23:02 +0000

    task 10

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -2,16 +2,16 @@
 
 const updateUniqueItems = (map) => {
     if (!(map instanceof Map)) {
-      throw new Error('Cannot process');
+        throw new Error('Cannot process');
     }
-  
+
     for (const [key, value] of map) {
-      if (value === 1) {
+        if (value === 1) {
         map.set(key, 100);
-      }
+        }
     }
-  
+
     return map;
-  };
+};
   
 export default updateUniqueItems;
